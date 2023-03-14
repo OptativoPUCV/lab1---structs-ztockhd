36101405de4ed58fad48cea3893b33b89df7714a
Replit user <>
2023-03-14 17:21:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -18,7 +18,7 @@ void swap(int *a, int *b)
   int auxA = *a;
   *a = *b;
   *b = auxA;
-  
+  /*ezzzzzzzzzzz
 }
 
 /*
@@ -27,8 +27,9 @@ Programe la función void arrayMaxMin(int *a, int n, int *max, int *min),
 la cual encuentra el máximo y el mínimo valor del arreglo a y los
 almacena en las variables apuntadas por max y min.
 */
-void arrayMaxMin(int *a, int n, int *max, int *min) {
-    
+void arrayMaxMin(int *a, int n, int *max, int *min) 
+{
+  
 }
 
 
